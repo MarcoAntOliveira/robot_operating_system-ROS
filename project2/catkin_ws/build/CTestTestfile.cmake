b5b88b70bb214d73df4bf927dea106277151f26a
MarcoAntOliveira <marcoolivera096@gmail.com>
2024-10-14 10:41:29 -0300

    adicionando project2

---
+++ project2/catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/marcos/projects/ROS/catkin_ws/src
+# Build directory: /home/marcos/projects/ROS/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("my_project_py")
---
+++ project2/catkin_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/marcos/projects/ROS/catkin_ws/src'
+whitelisted_packages = 'my_project_py'.split(';') if 'my_project_py' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/opt/ros/noetic'.split(';') if '/opt/ros/noetic' != '' else []
