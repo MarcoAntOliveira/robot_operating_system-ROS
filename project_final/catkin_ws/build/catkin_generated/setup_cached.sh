46d2eef8e8b5fbf56ff8f72a26b53811bdcaf742
MarcoAntOliveira <marcoolivera096@gmail.com>
2024-10-21 09:01:36 -0300

    adicionando o projeto final

---
+++ project_final/catkin_ws/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/marcos/projects/ROS/robot_operating_system-ROS/project_final/catkin_ws/devel:$CMAKE_PREFIX_PATH"
+export PWD='/home/marcos/projects/ROS/robot_operating_system-ROS/project_final/catkin_ws/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/marcos/projects/ROS/robot_operating_system-ROS/project_final/catkin_ws/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/marcos/projects/ROS/robot_operating_system-ROS/project_final/catkin_ws/src:$ROS_PACKAGE_PATH"
